a7294cded15da28f7df1999bb22f78c3ff6d1084
Chloe Hughes <cahughes@stanford.edu>
2025-06-24 19:06:47 -0700

    update key for suppabase

--- src/lib/supabaseclient.js
+++ src/lib/supabaseclient.js
@@ -1,6 +1,6 @@
-// lib/supabaseClient.js or in a top-level utils file
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://gttvhadmroffvyscotjr.supabase.co'
-const supabaseKey = 'your-eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0dHZoYWRtcm9mZnZ5c2NvdGpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3OTIyNTAsImV4cCI6MjA2NjM2ODI1MH0.InaM3k96KFBjsChkwAK5FZcByDMipBpbcF-GKd_T4ZU-public-key'
-export const supabase = createClient(supabaseUrl, supabaseKey)
+const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnon)
